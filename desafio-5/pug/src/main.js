434827486fe8cee61eac7ffa5ff53d10d7ec2215
johnatan-ferre <johnatanferre@gmail.com>
2022-08-16 22:59:40 -0300

    desafio-5, motores de plantillas

---
+++ desafio-5/pug/src/main.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const ItemsApi = require('../api/items')
+
+const itemsApi = new ItemsApi()
+
+let existenciaItems = false
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+
+app.post('/productos', (req, res) => {
+    itemsApi.save(req.body)
+    res.redirect('/')
+})
+
+app.get('/productos', (req, res) => {
+    const items = itemsApi.listAll()
+    existenciaItems = items.length > 0 ? true : false
+    res.render('vista' {items, existenciaItems})
+})
+
+const PORT = 8080
+const server = app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el ${server.address().port}`)
+})
+server.on("error", error => console.log(`Error en servidor ${error}`))
